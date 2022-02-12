4bc2f94307f8df7d6a2945d44354c4e62f30dfc8
U160709013 <remzibulutlu@posta.mu.edu.tr>
2022-02-12 23:24:42 +0000

    improved test

--- simulink_example_cmake/bacteria-simulink/test/halil_function_test.cpp
+++ simulink_example_cmake/bacteria-simulink/test/halil_function_test.cpp
@@ -2,6 +2,14 @@
 #include <our_model.h>
 #include "gtest/gtest.h"
 
-TEST(our_model_prevModelClass, halil_function_test) {
+TEST(halil, bir_gonder_iki_al) {
   EXPECT_EQ(2, halil(1));
+}
+
+TEST(halil, dort_gonder_alti_al) {
+  EXPECT_EQ(6, halil(4));
+}
+
+TEST(halil, uc_gonder_dort_al) {
+  EXPECT_EQ(4, halil(3));
 }
